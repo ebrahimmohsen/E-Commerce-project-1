cb1d506b845d3264ea8f55d52930a558c1a6fffe
ebrahimmohsen <ebrahimzaki456@gmail.com>
2025-11-09 01:31:15 +0200

    Api2

---
+++ Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Persistence.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork(StoreDbContext _dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> _repositories = new Dictionary<string, object>();
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            var typeName = typeof(TEntity).Name;
+            if (_repositories.ContainsKey(typeName))
+            {
+                return (IGenericRepository<TEntity, TKey>)_repositories[typeName];
+            }
+            // Create a repo object
+            var repo = new GenericRepository<TEntity, TKey>(_dbContext);
+            // store reference from repo object 
+            _repositories[typeName]=repo;
+            // return repo object
+            return repo;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+
+        }
+    }
+}
